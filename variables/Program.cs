62758ed419939eae00351d5c395916d16fe48f65
Juan Obando <juanobando.developer@gmail.com>
2023-07-12 13:33:38 -0500

    feat: calculate the are of circle using constants

--- variables/Program.cs
+++ variables/Program.cs
@@ -3,12 +3,22 @@ int sideA; // explicit declaration of the type
 int sideB;
 // with var we can declare a variable without specifying the type
 // and the compiler will infer the type based on the value assigned
-var result = 0.0; // implicit declaration of the type
+var resultRectangle = 0.0; // implicit declaration of the type
 
 Console.WriteLine("Calculate the area of a rectangle");
 Console.WriteLine("Enter the value of side A");
 sideA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter the value of side B");
 sideB = Convert.ToInt32(Console.ReadLine());
-result = sideA * sideB;
-Console.WriteLine("The area of the rectangle is: " + result + "cm2");
+resultRectangle = sideA * sideB;
+Console.WriteLine("The area of the rectangle is: " + resultRectangle + "cm2");
+
+// calculating the area of a circle
+var resultCircle = 0d; // with d or f we can specify the type of the variable between double or float
+const double Pi = 3.14159265359; // with const we can declare a constant variable
+Console.WriteLine("Calculate the area of a circle");
+Console.WriteLine("Enter the value of the radius");
+double radius = Convert.ToDouble(Console.ReadLine());
+resultCircle = Pi * Math.Pow(radius, 2);
+Console.WriteLine("The area of the circle is: " + resultCircle + "cm2");
+
