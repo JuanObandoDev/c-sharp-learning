6590c6f7a6ebfd57faaf940cfab69e8e11af005e
Juan Obando <juanobando.developer@gmail.com>
2023-07-12 13:18:39 -0500

    feat: create variables folder to learn about variables on c#

---
+++ variables/Program.cs
@@ -0,0 +1,12 @@
+﻿// program that calculate the area of a rectangle
+int sideA;
+int sideB;
+int result;
+
+Console.WriteLine("Calculate the area of a rectangle");
+Console.WriteLine("Enter the value of side A");
+sideA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the value of side B");
+sideB = Convert.ToInt32(Console.ReadLine());
+result = sideA * sideB;
+Console.WriteLine("The area of the rectangle is: " + result + "cm2");
